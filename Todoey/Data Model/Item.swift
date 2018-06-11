4a3a37ffc8241dc895d8843dfeffd56be2e005ff
Wisnu Anggoro <wisnu@anggoro.net>
2018-06-11 14:15:05 +0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by MTMAC18 on 11/06/18.
+//  Copyright © 2018 Wisnu Anggoro. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
